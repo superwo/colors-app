925fc6358a6751612929b2e6d850911b7bc627b8
Radu <avadanii@tiscali.it>
2019-06-25 16:25:54 +0300

    added single color route

--- src/App.js
+++ src/App.js
@@ -16,7 +16,9 @@ class App extends Component {
         <Route
           exact
           path='/'
-          render={() => <PaletteList palettes={seedColors} />}
+          render={routeProps => (
+            <PaletteList palettes={seedColors} {...routeProps} />
+          )}
         />
         <Route
           exact
@@ -28,6 +30,11 @@ class App extends Component {
               )}
             />
           )}
+        />
+        <Route
+          exact
+          path='/palette/:paletteId/:colorId'
+          render={() => <h1>SINGLE COLOR PAGE</h1>}
         />
       </Switch>
       // <div>
